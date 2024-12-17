a90ad91ceb4cc0e95b0afe68b5c3f8a31d5d3442
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-12-17 18:49:55 +0530

    choclate array added

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ app.get("/ping", (req, res)=>{
     res.send("pong")
 })
 
+const CHOCOLATES = [
+    {
+      id: 1,
+      name: "Dairy Milk",
+      description: "Creamy milk chocolate",
+    },
+    {
+      id: 2,
+      name: "KitKat",
+      description: "Crispy wafer chocolate",
+    },
+    {
+      id: 3,
+      name: "Ferrero Rocher",
+      description: "Hazelnut chocolate ball",
+    },
+    {
+      id: 4,
+      name: "Galaxy",
+      description: "Smooth and creamy chocolate",
+    },
+    {
+      id: 5,
+      name: "Snickers",
+      description: "Peanut and caramel chocolate bar",
+    },
+  ];
+  
+
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
